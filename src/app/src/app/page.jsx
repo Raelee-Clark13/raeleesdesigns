718a0f0d4efd515e9196337846f93931216a38fb
Raelee-Clark13 <raelee@raeleesdesign.com>
2025-10-27 18:13:34 -0600

    Delete src/app/src directory

--- src/app/src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Raelee’s Portfolio Assistant</h1>
-      <p>First deploy test. If you can read this, it works.</p>
-    </main>
-  );
-}
